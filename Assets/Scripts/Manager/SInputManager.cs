685c2c75168e41dc26f5e8fd1f1f5ce209609146
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-02-21 08:01:50 +0200

    Added SInputManager

---
+++ Assets/Scripts/Manager/SInputManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+namespace Kekw.Manager
+{
+    /// <summary>
+    /// Class handler input device finding and distributes it for everybody in the scene.<br></br><br></br>
+    /// Class is singleton and does not destroyOnLoad.
+    /// </summary>
+    class SInputManager
+    {
+
+    }
+}
