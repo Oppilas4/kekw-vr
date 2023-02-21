ca705a2dd79b3bd16aa0b8a4a0de830c82006f64
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-02-21 12:23:08 +0200

    Added hand activation script

---
+++ Assets/Scripts/Interaction/HandActivation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.InputSystem;
+
+namespace Kekw.Interaction
+{
+    /// <summary>
+    /// Hand activation control.
+    /// </summary>
+    class HandActivation: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Left hand")]
+        GameObject leftHand;
+
+        [SerializeField]
+        [Tooltip("Right hand")]
+        GameObject RightHand;
+
+        InputActionManager inputActionManager;
+        InputAction handActivation;
+
+        private bool handsActive;
+
+        private void Awake()
+        {
+            // Get reference to InputActionManager
+            inputActionManager = FindObjectOfType<InputActionManager>();
+
+            // handactivation input action
+            handActivation = inputActionManager.actionAssets[0].FindActionMap("XRI RightHand").FindAction("HandActivate");
+
+            // Attach event for hand activation input action performed.
+            handActivation.performed += OnHandActivated;
+            handsActive = false;
+        }
+
+        /// <summary>
+        /// Sets hands active or deactive.
+        /// </summary>
+        /// <param name="context"></param>
+        private void OnHandActivated(InputAction.CallbackContext context)
+        {
+            handsActive = !handsActive;
+            RightHand.SetActive(handsActive);
+            leftHand.SetActive(handsActive);
+        }
+    }
+}
