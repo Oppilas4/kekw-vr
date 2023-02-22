4052ec0525df75b37bf1c08f756568b59b50c1db
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-02-22 14:32:54 +0200

    Updated coding mission with assets

--- Assets/Scripts/Mission/CodingMissionManager.cs
+++ Assets/Scripts/Mission/CodingMissionManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
 namespace Kekw.Mission
 {
@@ -21,12 +23,37 @@ namespace Kekw.Mission
         [Tooltip("Fill rate")]
         float _fillRate;
 
+        [SerializeField]
+        [Tooltip("Success texture")]
+        Texture _successTexture;
+
+        [SerializeField]
+        [Tooltip("Fail texture")]
+        Texture _FailTexture;
+
+        [SerializeField]
+        [Tooltip("Game running texture")]
+        Texture _IgTexture;
+
+        [SerializeField]
+        [Tooltip("Standby texture")]
+        Texture _OriginalTexture;
+
+        [SerializeField]
+        [Tooltip("Fail and success target texture display")]
+        GameObject _targetDisplay;
+        MeshRenderer _targetDisplayMeshRenderer;
+
         private bool _isActive;
+
+        Coroutine _originalTextureSetter;
 
         private void Awake()
         {
             // Get reference to material/shader.
             _material = _lineRenderer.materials[0];
+            _targetDisplayMeshRenderer = _targetDisplay.GetComponent<MeshRenderer>();
+            _isActive = false;
         }
 
         private void Update()
@@ -50,9 +77,13 @@ namespace Kekw.Mission
         /// </summary>
         public void OnMissionStart()
         {
-            // Set shader time multiplier.
-            _material.SetFloat("_TimeMultiplier", _speed);
-            _isActive = true;
+            if (!_isActive)
+            {
+                if (_originalTextureSetter != null) StopCoroutine(_originalTextureSetter);
+                _material.SetFloat("_TimeMultiplier", _speed);
+                _isActive = true;
+                SetDisplayTexture(_IgTexture);
+            }
         }
 
         /// <summary>
@@ -60,18 +91,21 @@ namespace Kekw.Mission
         /// </summary>
         public void OnMissionStop()
         {
-            // if shader _OffsetX is in last 20% mission is success
-            if (_material.GetFloat("_OffsetX") > .01 && _material.GetFloat("_OffsetX") < .2f)
+            if (_isActive)
             {
-                OnMissionSuccess();
+                _isActive = false;
+                // if shader _OffsetX is in last 20% mission is success
+                if (_material.GetFloat("_OffsetX") > .01 && _material.GetFloat("_OffsetX") < .2f)
+                {
+                    OnMissionSuccess();
+                }
+                else
+                {
+                    OnMissionFail();
+                }
+                _material.SetFloat("_OffsetX", 1.1f);
+                _material.SetFloat("_TimeMultiplier", 0f);
             }
-            else
-            {
-                OnMissionFail();
-            }
-            _isActive = false;
-            _material.SetFloat("_OffsetX", 1.1f);
-            _material.SetFloat("_TimeMultiplier", 0f);
         }
 
         /// <summary>
@@ -79,7 +113,8 @@ namespace Kekw.Mission
         /// </summary>
         public void OnMissionFail()
         {
-            Debug.Log("Mission failed");
+            SetDisplayTexture(_FailTexture);
+            _originalTextureSetter = StartCoroutine(SetOriginalTexture());
         }
 
         /// <summary>
@@ -87,7 +122,24 @@ namespace Kekw.Mission
         /// </summary>
         public void OnMissionSuccess()
         {
-            Debug.Log("Mission succeess");
+            SetDisplayTexture(_successTexture);
+            _originalTextureSetter = StartCoroutine(SetOriginalTexture());
         }
-    } 
+
+        IEnumerator SetOriginalTexture()
+        {
+            yield return new WaitForSeconds(5);
+            SetDisplayTexture(_OriginalTexture);
+        }
+
+        /// <summary>
+        /// Set display texture.
+        /// </summary>
+        /// <param name="texture">Texture to set</param>
+        private void SetDisplayTexture(Texture texture)
+        {
+            _targetDisplayMeshRenderer.material.mainTexture = texture;
+            _targetDisplayMeshRenderer.material.SetTexture("_EmissionMap", texture);
+        }
+    }
 }
