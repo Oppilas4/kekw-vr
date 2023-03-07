e0b20d7ab97295db01bff357340346a6127cd4d4
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-07 09:21:58 +0200

    Re organising

---
+++ Assets/Scripts/Interaction/IngameButton/IGButtonTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Kekw.Interaction;
+
+namespace Kekw.Interaction.Igbuttontest
+{
+    /// <summary>
+    /// Simple test case for ButtonTestScene
+    /// </summary>
+    class IGButtonTest : MonoBehaviour, IIngameButtonLogic
+    {
+        public void TriggerAction()
+        {
+            Debug.Log("Button pressed, KEKW!");
+        }
+    }
+}
