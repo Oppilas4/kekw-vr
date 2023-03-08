2b12005934f61ab8d843d92bfe491e2f990e9396
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-08 15:13:17 +0200

    Continued VuoksiBot

---
+++ Assets/Scripts/VuoksiBotti/AnimationManager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Kekw.VuoksiBotti
+{
+    class AnimationManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Helper struct to identify animation layers
+        /// </summary>
+        [Serializable]
+        internal struct LayerData{
+            public int indexBegin;
+            public int indexEnd;
+        }
+
+
+        [SerializeField]
+        [Tooltip("Animator to control")]
+        Animator _animator;
+
+        [SerializeField]
+        [Tooltip("Base animation layers index range")]
+        LayerData _baseLayerRange;
+
+        [SerializeField]
+        [Tooltip("Random dance move layer index range")]
+        LayerData _danceLayerRange;
+
+        [SerializeField]
+        [Tooltip("Talking layer index range")]
+        LayerData _talkingLayerRange;
+
+        [SerializeField]
+        [Tooltip("Random dance change %")]
+        int _randomDanceChance = 30;
+
+        [SerializeField]
+        [Tooltip("Random dance time in seconds")]
+        Vector2Int _danceTimeDelta;
+
+        /// <summary>
+        /// Is character talking to player
+        /// </summary>
+        public bool IsTalking { get; set; } = false;
+
+
+        Coroutine activeTimer;
+        bool _isPLayingRandomDance = false;
+
+        private void Start()
+        {
+            // Reset all additional layers to 0 except base.
+            this.SetLayerWeights(_danceLayerRange, 0);
+            this.SetLayerWeights(_talkingLayerRange, 0);
+        }
+
+        private void Update()
+        {
+            // Randomize character animation if not talking.
+            if (!IsTalking)
+            {
+                if (!_isPLayingRandomDance)
+                {
+                    _isPLayingRandomDance = true;
+                    if(activeTimer != null) StopCoroutine(activeTimer);
+                    // Chance to play random dance default 30%
+                    if (UnityEngine.Random.Range(1, 101) <= _randomDanceChance)
+                    {
+                        this.SetRandomDanceWeight();
+                        activeTimer = StartCoroutine(ResetDance(UnityEngine.Random.Range(_danceTimeDelta.x, _danceTimeDelta.y)));
+                    }
+                    else
+                    {
+                        this.SetLayerWeights(_danceLayerRange, 0);
+                        activeTimer = StartCoroutine(ResetDance(UnityEngine.Random.Range(_danceTimeDelta.x, _danceTimeDelta.y)));
+                    }
+                }
+            }
+            else
+            {
+                this.SetTalkMode();
+            }
+        }
+
+        /// <summary>
+        /// Set character to talking mode
+        /// </summary>
+        private void SetTalkMode()
+        {
+            this.SetLayerWeights(_talkingLayerRange, 1);
+        }
+
+        /// <summary>
+        /// Sets random dance layer to weight of 1. Character picks up random dance animation.
+        /// </summary>
+        private void SetRandomDanceWeight()
+        {
+            this.SetLayerWeights(_danceLayerRange, 0);
+            int danceIndex = UnityEngine.Random.Range(_danceLayerRange.indexBegin, _danceLayerRange.indexEnd + 1);
+            _animator.SetLayerWeight(danceIndex, 1);
+        }
+
+        /// <summary>
+        /// Set weight all layers in range
+        /// </summary>
+        /// <param name="layers"></param>
+        /// <param name="weight"></param>
+        private void SetLayerWeights(LayerData layers ,int weight)
+        {
+            for (int i = layers.indexBegin; i <= layers.indexEnd; i++)
+            {
+                _animator.SetLayerWeight(i, weight);
+            }
+        }
+
+        /// <summary>
+        /// Coroutine to reset dance back to base after "time" seconds;
+        /// </summary>
+        /// <param name="time">Seconds before reset</param>
+        /// <returns></returns>
+        IEnumerator ResetDance(int time)
+        {
+            yield return new WaitForSeconds(time);
+            this.SetLayerWeights(_danceLayerRange, 0);
+            _isPLayingRandomDance = false;
+        }
+    }
+}
--- Assets/Scripts/VuoksiBotti/Mover.cs
+++ Assets/Scripts/VuoksiBotti/Mover.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace Kekw.VuoksiBotti
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Handles bot moving and routing.
+    /// </summary>
+    public class Mover : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        [Tooltip("Baked navmesh plane data(nav mesh data) where we want random navigation points to occur.")]
+        NavMeshData[] _navMeshDatas;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        NavMeshAgent _navMeshAgent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _navMeshAgent = this.GetComponent<NavMeshAgent>();
+            _navMeshAgent.path = GetNewPath();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(_navMeshAgent.remainingDistance < .1f)
+            {
+                _navMeshAgent.path = GetNewPath();
+            }
+        }
+
+        /// <summary>
+        /// Gets random navigation area data and draw random points until path is valid.
+        /// </summary>
+        /// <returns></returns>
+        private NavMeshPath GetNewPath()
+        {
+            int randomNavMeshDataIndex = Random.Range(0, _navMeshDatas.Length);
+            // Get random points while they are not valid
+            while (true)
+            {
+                Vector3 randomPoint = new Vector3(
+                   Random.Range(_navMeshDatas[randomNavMeshDataIndex].sourceBounds.min.x, _navMeshDatas[randomNavMeshDataIndex].sourceBounds.max.x),
+                   Random.Range(_navMeshDatas[randomNavMeshDataIndex].sourceBounds.min.y, _navMeshDatas[randomNavMeshDataIndex].sourceBounds.max.y),
+                   Random.Range(_navMeshDatas[randomNavMeshDataIndex].sourceBounds.min.z, _navMeshDatas[randomNavMeshDataIndex].sourceBounds.max.z)
+                   );
+                // Check point is in mesh and can be navigated to
+                NavMeshPath path = new NavMeshPath();
+                _navMeshAgent.CalculatePath(randomPoint, path);
+                if(path.status == NavMeshPathStatus.PathComplete)
+                {
+                    return path;
+                }
+            }
+        }
+    } 
 }
