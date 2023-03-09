4b0a1bb3fb3b9d228e85f878544ad9e1ecc9efab
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-09 13:02:11 +0200

    Switched bot to new input action map

---
+++ Assets/Scripts/Interaction/InputMapInitializer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+namespace Kekw.Interaction
+{
+    /// <summary>
+    /// Disables bot interaction map at the beginning of the play.
+    /// </summary>
+    class InputMapInitializer: MonoBehaviour
+    {
+        InputActionManager _inputActionManager;
+
+        /// <summary>
+        /// Default action map used for left hand.
+        /// </summary>
+        public static InputActionMap NormalLeftHandActionMap { get; private set; }
+       
+        /// <summary>
+        /// Special action map that is only used near the robot.
+        /// </summary>
+        public static InputActionMap BotLeftHandActionMap { get; private set; }
+
+        private void Awake()
+        {
+            // find input action manager and disable bot input action map.
+            _inputActionManager = FindObjectOfType<InputActionManager>();
+            BotLeftHandActionMap = _inputActionManager.actionAssets[0].FindActionMap("XRI RightHand_botti");
+            NormalLeftHandActionMap = _inputActionManager.actionAssets[0].FindActionMap("XRI RightHand");
+            BotLeftHandActionMap.Disable();
+        }
+    }
+}
---
+++ Assets/Scripts/VuoksiBotti/BotInteractionDetector.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using Kekw.Interaction;
+using System;
+using UnityEngine.InputSystem;
+
+namespace Kekw.VuoksiBotti
+{
+    /// <summary>
+    /// Handles input action switching and bot human interface.
+    /// </summary>
+    class BotInteractionDetector: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Boombox")]
+        BoomBox _boomBox;
+
+        [SerializeField]
+        [Tooltip("Mover component")]
+        Mover _mover;
+
+        [SerializeField]
+        [Tooltip("Robot animation manager")]
+        AnimationManager _animationManager;
+
+        [SerializeField]
+        [Tooltip("Talk component")]
+        Talk _talk;
+
+
+        private void OnTriggerStay(Collider other)
+        {
+            // Activate bot input action map when near robot.
+            if (other.gameObject.CompareTag("Player"))
+            {
+                InputMapInitializer.NormalLeftHandActionMap.Disable();
+                InputMapInitializer.BotLeftHandActionMap.Enable();
+                InputMapInitializer.BotLeftHandActionMap.FindAction("BotPause").performed += SetBotToPause;
+                InputMapInitializer.BotLeftHandActionMap.FindAction("BotTalk").performed += SetBotToTalk;
+
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            // DeActivate bot input action map when left robot proximity.
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("Player has left bot assssss");
+                InputMapInitializer.BotLeftHandActionMap.FindAction("BotPause").performed -= SetBotToPause;
+                InputMapInitializer.BotLeftHandActionMap.FindAction("BotTalk").performed -= SetBotToTalk;
+                InputMapInitializer.NormalLeftHandActionMap.Enable();
+                InputMapInitializer.BotLeftHandActionMap.Disable();
+            }
+        }
+
+        private void SetBotToTalk(InputAction.CallbackContext obj)
+        {
+            _talk.TalkSingle();
+        }
+
+        private void SetBotToPause(InputAction.CallbackContext obj)
+        {
+            _talk.SetPause();
+            _animationManager.SetPause();
+            _boomBox.SetPause();
+            _mover.SetPause();
+        }
+
+        
+    }
+}
