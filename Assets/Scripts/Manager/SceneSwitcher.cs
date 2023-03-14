16512809fdb2f657d35e145232c4ffdd7867681d
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-14 14:06:30 +0200

    Updated positioning on scene change

--- Assets/Scripts/Manager/SceneSwitcher.cs
+++ Assets/Scripts/Manager/SceneSwitcher.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Playables;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 namespace Kekw.Manager
 {
@@ -22,6 +21,11 @@ namespace Kekw.Manager
         [Tooltip("OUT playable asset")]
         PlayableAsset _outTransition;
 
+        [SerializeField]
+        [Tooltip("Default jump position in scenes")]
+        Vector3 _defaultJumpPosition;
+
+        Vector3 _sceneJumpPosition;
         string _sceneToLoad;
 
         AsyncOperation sceneLoadOperation;
@@ -35,16 +39,30 @@ namespace Kekw.Manager
 
             if (Input.GetKeyUp(KeyCode.Z))
             {
-                SwitchScene("World_Dev");
+                SetSceneJumpPosition(new Vector3(0f,.1f, 0f));
+                SwitchScene("World_Dev", false);
             }
+        }
+
+        /// <summary>
+        /// Set player position in new scene. Set BEFORE scene change.
+        /// </summary>
+        /// <param name="position"></param>
+        public void SetSceneJumpPosition(Vector3 position)
+        {
+            this._sceneJumpPosition = position;
         }
 
         /// <summary>
         /// Switch scene after playing post processing animations.
         /// </summary>
         /// <param name="sceneName">Scene name</param>
-        public void SwitchScene(string sceneName)
+        public void SwitchScene(string sceneName, bool useDefaultPosition = true)
         {
+            if (useDefaultPosition)
+            {
+                _sceneJumpPosition = _defaultJumpPosition;
+            }
             this._sceneToLoad = sceneName;
             _ppDirector.time = 0f;
             _ppDirector.Play(_inTransition);
@@ -71,6 +89,9 @@ namespace Kekw.Manager
         {
             if (operation.isDone && sceneLoadOperation != null)
             {
+                // Update player to new position in scene while screen is black.
+                PlayerSingleton.Instance.gameObject.transform.position = _sceneJumpPosition;
+                // Start scene fading.
                 _ppDirector.Play(_outTransition);
                 sceneLoadOperation.completed -= OnSceneLoadComplete;
                 sceneLoadOperation = null;
