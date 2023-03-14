d54d2cd19a1228b439cd94961f0bfac4293d6af6
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-14 10:28:21 +0200

    Added cam transitions

---
+++ Assets/Scripts/Manager/NonDestructable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Manager
+{
+    class NonDestructable: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Root gameobject")]
+        GameObject _root;
+        
+        private void Awake()
+        {
+            DontDestroyOnLoad(_root);
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/SceneSwitcher.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+using System.Collections;
+
+namespace Kekw.Manager
+{
+    /// <summary>
+    /// Scene switching with transition animations.
+    /// </summary>
+    class SceneSwitcher: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("PP aniator")]
+        PlayableDirector _ppDirector;
+
+        [SerializeField]
+        [Tooltip("IN playable asset")]
+        PlayableAsset _inTransition;
+
+        [SerializeField]
+        [Tooltip("OUT playable asset")]
+        PlayableAsset _outTransition;
+
+
+        string _sceneToLoad;
+
+
+        /// <summary>
+        /// Switch scene after playing post processing animations.
+        /// </summary>
+        /// <param name="sceneName">Scene name</param>
+        public void SwitchScene(string sceneName)
+        {
+            this._sceneToLoad = sceneName;
+            _ppDirector.Play(_inTransition);
+            _ppDirector.played += DoSceneSwitch;
+        }
+        
+        /// <summary>
+        /// Change scene after transition complete.
+        /// </summary>
+        /// <param name="obj"></param>
+        private void DoSceneSwitch(PlayableDirector director)
+        {
+            AsyncOperation sceneLoadOperation = SceneManager.LoadSceneAsync(_sceneToLoad);
+            sceneLoadOperation.completed += OnSceneLoadComplete;
+        }
+        
+        /// <summary>
+        /// Scene load async operation complete.
+        /// </summary>
+        /// <param name="operation"></param>
+        private void OnSceneLoadComplete(AsyncOperation operation)
+        {
+            if (operation.isDone)
+            {
+                _ppDirector.Play(_outTransition);
+            }
+        }
+
+        IEnumerator Test()
+        {
+            yield return new WaitForSeconds(5);
+            SwitchScene("Empty");
+        }
+        
+    }
+}
