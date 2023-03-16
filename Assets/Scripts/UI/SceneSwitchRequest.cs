7c96e76961b1bee1b080d95729012755615da045
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-16 10:44:59 +0200

    Added teleport to auditorio

---
+++ Assets/Scripts/UI/SceneSwitchRequest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Kekw.Manager;
+
+namespace Kekw.UI
+{
+    /// <summary>
+    /// Component canbe hookes to button to request scene switch from <seealso cref="SceneSwitcher"/>
+    /// </summary>
+    class SceneSwitchRequest:MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("New scene where we should teleport")]
+        string _sceneName;
+
+        [SerializeField]
+        [Tooltip("Player position in new scene")]
+        Vector3 _newPlayerPosition;
+
+        [SerializeField]
+        [Tooltip("Ignore new player position in future scene")]
+        bool _ignoreNewposition = false;
+
+        /// <summary>
+        /// Change scene with location setting.
+        /// </summary>
+        /// <param name="sceneName"></param>
+        /// <param name="newPlayerPosition"></param>
+        public void OnUiElementClicked()
+        {
+            SceneSwitcher sceneSwitcher = FindObjectOfType<SceneSwitcher>();
+            if (_ignoreNewposition)
+            {
+                sceneSwitcher.SwitchScene(_sceneName);
+            }
+            else
+            {
+                sceneSwitcher.SetSceneJumpPosition(_newPlayerPosition);
+                sceneSwitcher.SwitchScene(_sceneName, false);
+            }
+        }
+    }
+}
