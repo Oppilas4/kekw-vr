8c881f58ae39163124b5eed71869a4dd7f68cdd9
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-23 11:42:15 +0200

    Added desi desi kasi

---
+++ Assets/Scripts/Interaction/IngameButton/DisinfectionHolder.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kekw.Interaction
+{
+    public class DisinfectionHolder : MonoBehaviour, IIngameButtonLogic
+    {
+        [SerializeField]
+        [Tooltip("VFX effect to Instantiate when button is pressed")]
+        GameObject _disinfectionVFX;
+
+        [SerializeField]
+        [Tooltip("Barrel where stuff flows out")]
+        GameObject _barrel;
+
+        GameObject _trackedObject;
+        bool _firstCollision = true;
+
+        /// <summary>
+        /// <seealso cref="IIngameButtonLogic"/>
+        /// </summary>
+        public void TriggerAction()
+        {
+            if(_trackedObject == null && !_firstCollision)
+            {
+                _trackedObject = Instantiate(_disinfectionVFX);
+                _trackedObject.transform.position = _barrel.transform.position;
+                _trackedObject.transform.rotation = _barrel.transform.rotation;
+                Destroy(_trackedObject.gameObject, .8f);
+            }
+            else
+            {
+                _firstCollision = false;
+            }
+        }
+
+        void Update()
+        {
+            if(_trackedObject != null)
+            {
+                _trackedObject.transform.position = _barrel.transform.position;
+            }    
+        }
+    } 
+}
