6182cf455bd5a00b6bbd90ae89460c59577b7833
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-03-29 14:22:16 +0300

    Added drink spawning

---
+++ Assets/Scripts/Pool/DrinkPool/DrinkPool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kekw.Pool
+{
+    /// <summary>
+    /// Pool for all the drinks for 'random' spawning.
+    /// </summary>
+    class DrinkPool: APool
+    {
+        [SerializeField]
+        [Tooltip("Array of available drinks in game.")]
+        GameObject[] _drinkPoolObjects;
+
+        protected override void InitializePool()
+        {
+            _pool = new Queue<APoolMember>();
+            // initialize pool
+            for (int i = 0; i < _size; i++)
+            {
+                GameObject temp = Instantiate(_drinkPoolObjects[Random.Range(0, _drinkPoolObjects.Length)]);
+                temp.SetActive(false);
+                temp.transform.position = this.transform.position;
+                _pool.Enqueue(temp.GetComponent<APoolMember>());
+            }
+        }
+    }
+}
