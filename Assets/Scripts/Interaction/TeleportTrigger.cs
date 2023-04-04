77161be5c501fe5ec955260eea358cdfaddf2f5a
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-04 12:56:54 +0300

    Teleport test

---
+++ Assets/Scripts/Interaction/TeleportTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Kekw.Interaction
+{
+    /// <summary>
+    /// Detecs player entering to teleport area.
+    /// </summary>
+    class TeleportTrigger: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Gameobject to activate when player enters the trigger.")]
+        GameObject _target;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                _target.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                _target.SetActive(false);
+            }
+        }
+    }
+}
