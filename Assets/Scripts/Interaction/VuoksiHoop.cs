d9dab7a30f9939d047f563f7526d5f0cccaf45bb
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-05 13:42:02 +0300

    Added vuoksi ballz lights and stuff

---
+++ Assets/Scripts/Interaction/VuoksiHoop.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+namespace Kekw.Interaction
+{
+    /// <summary>
+    /// Vuoksi hoop ball detector
+    /// </summary>
+    class VuoksiHoop: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Goal sound")]
+        AudioSource _goalSound;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Ball"))
+            {
+                _goalSound.PlayOneShot(_goalSound.clip);
+            }
+        }
+    }
+}
