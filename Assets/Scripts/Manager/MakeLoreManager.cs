e1624c7e7df6c29efa022c5c46d188684afd50a5
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-12 09:33:36 +0300

    Added make audio

---
+++ Assets/Scripts/Manager/MakeLoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kekw.Manager
+{
+    class MakeLoreManager: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("BGR audio source")]
+        AudioSource _bgrSource;
+
+        [SerializeField]
+        [Tooltip("Teleport thing to activate")]
+        GameObject _teleport;
+
+
+        public void OnLoreEnd()
+        {
+            _bgrSource.Play();
+            _teleport.SetActive(true);
+        }
+    }
+}
