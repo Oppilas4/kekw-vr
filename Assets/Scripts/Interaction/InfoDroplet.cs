b5fe8e1f43a08bff9160193b796741f0e12ca450
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-18 07:33:00 +0300

    Added vuoksi droplet and moved player spawn point

---
+++ Assets/Scripts/Interaction/InfoDroplet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kekw.Interaction
+{
+    /// <summary>
+    /// Info droplets tell player info about school
+    /// </summary>
+    class InfoDroplet: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Audio that this droplet plays")]
+        AudioSource _audioSource;
+
+        [SerializeField]
+        [Tooltip("Animator for this droplet")]
+        Animator _animator;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // Start talkin when player enters trigger
+            if (other.CompareTag("Player") && !_audioSource.isPlaying)
+            {
+                _audioSource.PlayOneShot(_audioSource.clip);
+                _animator.SetBool("Talking", true);
+                StartCoroutine(ResetTalkAnimation(_audioSource.clip.length));
+            }
+        }
+
+        /// <summary>
+        /// Reset talking animation after clip is played
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        IEnumerator ResetTalkAnimation(float time)
+        {
+            yield return new WaitForSecondsRealtime(time);
+            _animator.SetBool("Talking", false);
+        }
+    }
+}
