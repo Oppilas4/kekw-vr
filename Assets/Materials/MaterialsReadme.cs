d87cb08fa64f87071f679732bacfada522e36b6c
JereVierula <111411800+JereVierula@users.noreply.github.com>
2023-04-20 14:36:36 +0300

    Updated Readmes
    Added English versions to the Readmes.

--- Assets/Materials/MaterialsReadme.cs
+++ Assets/Materials/MaterialsReadme.cs
@@ -5,5 +5,15 @@ namespace Kekw.info
      
     Esimerkiksi jokaiselle tiilisein‰lle ei kannata tehd‰ omaa materiaalia vaan k‰ytt‰‰ samaa
     jaettua materiaalia ja skaalata materiaali sopivaksi UV-mapin avulla.
+
+    Nime‰mis m‰‰ritelm‰ olisi "MAT + objektin nimi" esim. "MATVendingMachine".
+
+
+    This folder contains all materials which are tileable and/or universal.
+
+    For instance it's not smart to create separate materials for every instance of the same chair but 
+    instead use the same material on all of them.
+
+    Naming Convention would be "MAT + object name" ex. "MATVendingMachine".
      */
 }
--- Assets/Models/ModelsReadme.cs
+++ Assets/Models/ModelsReadme.cs
@@ -6,12 +6,30 @@ namespace Kekw.info
      kansio sis‰lt‰‰ kaikki malliin liittyv‰t tiedostot
      - FBX
      - tesktuurit
-     - Materiaalit
+
+    Yrit‰ pit‰‰ jonkinlaista j‰rjestyst‰. Laita samankaltaiset objektit
+    samaan kansioon. esim. Bench2, Bench3 ja CafeChair ovat kaikki omissa 
+    alikansioissaan "Benches, chair&such" alikansiossa.
 
     HOX mallin prefab menee prefab kansioon ei t‰nne!
 
-    Jos mallissa k‰ytet‰‰n unityss‰ luotuja tileable materiaaleja, 
-    ne materiaalit lˆytyv‰t kansiosta materials.
+    Kun importtaat Blenderist‰ tai muusta niin varmista ett‰ 
+    mallilla ei ole mit‰‰n materiaalia. Materiaali tehd‰‰n Unityss‰.
+    Materiaalit ovat erillisess‰ "Materials" kansiossa.
+
+    ------------------------------------------------------------------------
+
+    This folder contains all imported FBX 3D-model files. Every model
+    should have it's own subfolder. Said subfolder should contain the model's:
+    -FBX-file
+    -Texture-files
+
+    Try to group similar objects together into the same subfolder. Otherwise it can be 
+    annoying to find them.
+
+    When importing models from Blender or elsewhere make sure it has no materials
+    during import. Materials are to be created in Unity via the textures.
+    Materials are stored in the separate "Materials" folder.
      */
 }
 
