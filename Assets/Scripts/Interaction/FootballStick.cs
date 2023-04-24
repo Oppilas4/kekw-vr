51434deb0d2960dedef37e1fdf56a4b599b295dd
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-24 16:30:13 +0300

    Table football works in theory....

--- Assets/Scripts/Interaction/FootballStick.cs
+++ Assets/Scripts/Interaction/FootballStick.cs
@@ -1,17 +1,45 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 namespace Assets.Scripts.Interaction
 {
+    public enum StickControlType
+    {
+        ROTATOR,
+        MOVER
+    }
+
     /// <summary>
     /// Football stick behaviour
+    /// Controls physical stick 
     /// </summary>
     class FootballStick: MonoBehaviour
     {
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.CompareTag("RightHand") || collision.gameObject.CompareTag("LeftHand"))
-            {
+        [SerializeField]
+        [Tooltip("Stick under control")]
+        Rigidbody _rigidbody;
 
+        [SerializeField]
+        [Tooltip("movement this controls")]
+        StickControlType _stickControlType;
+
+        [SerializeField]
+        [Tooltip("Emitted force")]
+        Vector3 _forceEmited;
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (other.gameObject.CompareTag("RightHand") || other.gameObject.CompareTag("LeftHand"))
+            {
+                if (_stickControlType == StickControlType.MOVER)
+                {
+                    _rigidbody.AddForce(_forceEmited, ForceMode.Impulse);
+                }
+                if (_stickControlType == StickControlType.ROTATOR)
+                {
+                    _rigidbody.AddTorque(_forceEmited, ForceMode.Impulse);
+                }
             }
         }
     }
