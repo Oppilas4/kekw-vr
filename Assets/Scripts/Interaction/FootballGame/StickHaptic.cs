c2af62bcfa98b7c4ec3c475b3a12d9f94b244f0c
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-25 12:58:49 +0300

    added haptics to football

---
+++ Assets/Scripts/Interaction/FootballGame/StickHaptic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kekw.Interaction.Football
+{
+    /// <summary>
+    /// Sends stick haptic when stick is touched... <--- ^_- nice
+    /// </summary>
+    class StickHaptic: MonoBehaviour
+    {
+        LeftHapticBroker _leftHapticBroker;
+        RightHapticBroker _rightHapticBroker;
+
+        private void Start()
+        {
+            _leftHapticBroker = FindObjectOfType<LeftHapticBroker>();
+            _rightHapticBroker = FindObjectOfType<RightHapticBroker>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.gameObject.CompareTag("RightHand"))
+            {
+                _rightHapticBroker.TriggerHapticFeedback(.5f, .1f);
+            }
+
+            if (collision.collider.gameObject.CompareTag("LeftHand"))
+            {
+                _leftHapticBroker.TriggerHapticFeedback(.5f, .1f);
+            }
+        }
+    }
+}
