e494c8829fa8c0b4af3b63e449336e2cb4654a21
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-25 14:00:20 +0300

    Fotball game should work

---
+++ Assets/Scripts/Interaction/FootballGame/BallReset.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kekw.Interaction.Football
+{
+    /// <summary>
+    /// Resets ball in football game
+    /// </summary>
+    class BallReset: MonoBehaviour, IIngameButtonLogic
+    {
+        Vector3 _orgPosition;
+        Rigidbody _rb;
+
+        private void Awake()
+        {
+            _orgPosition = this.transform.position;
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        /// <summary>
+        /// <seealso cref="IIngameButtonLogic"/>
+        /// </summary>
+        public void TriggerAction()
+        {
+            _rb.isKinematic = true;
+            this.transform.position = _orgPosition;
+            _rb.isKinematic = false;
+        }
+    }
+}
