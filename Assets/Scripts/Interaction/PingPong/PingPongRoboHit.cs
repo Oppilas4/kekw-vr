aaef3e40d1dc39262f7629b28fd2889a37be618b
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-26 14:01:28 +0300

    fix

--- Assets/Scripts/Interaction/PingPong/PingPongRoboHit.cs
+++ Assets/Scripts/Interaction/PingPong/PingPongRoboHit.cs
@@ -12,7 +12,7 @@ namespace Kekw.Interaction.PingPong
         {
             if (collision.gameObject.CompareTag("PingBall"))
             {
-                collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * PINGPONG_BOT_FORCE, ForceMode.Impulse);
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * PINGPONG_BOT_FORCE, ForceMode.VelocityChange);
             }
         }
     }
