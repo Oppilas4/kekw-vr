482d9e5ef07c943ad33b46daeec3dbfe2aed66d8
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-26 14:14:38 +0300

    Update PingPongRoboHit.cs

--- Assets/Scripts/Interaction/PingPong/PingPongRoboHit.cs
+++ Assets/Scripts/Interaction/PingPong/PingPongRoboHit.cs
@@ -4,15 +4,19 @@ namespace Kekw.Interaction.PingPong
 {
     class PingPongRoboHit:MonoBehaviour
     {
-        public float PINGPONG_BOT_FORCE = .3f;
-
-        Rigidbody _cache;
+        public float PINGPONG_BOT_FORCE = .15f;
 
         private void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.CompareTag("PingBall"))
-            {
-                collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * PINGPONG_BOT_FORCE, ForceMode.VelocityChange);
+            { 
+                // Test stopping ball at robot and "hitting" ----->
+                Rigidbody ballBody = collision.gameObject.GetComponent<Rigidbody>();
+                ballBody.isKinematic = true;
+                ballBody.velocity = Vector3.zero;
+                ballBody.isKinematic = false;
+                // <----- Test stopping ball at robot and "hitting"
+                ballBody.AddForce(Vector3.forward * PINGPONG_BOT_FORCE, ForceMode.Impulse);
             }
         }
     }
