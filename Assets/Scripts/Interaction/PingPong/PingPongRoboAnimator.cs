917c55269572aec57759da484b1a3b6e1fb96f61
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-26 09:26:13 +0300

    peperobohands

---
+++ Assets/Scripts/Interaction/PingPong/PingPongRoboAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kekw.Interaction.PingPong
+{
+    class PingPongRoboAnimator: MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Trigger to trigger")]
+        string _triggerName;
+
+        [SerializeField]
+        [Tooltip("Robo animator")]
+        Animator _roboAnimator;
+
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("PingBall"))
+            {
+                _roboAnimator.SetTrigger(_triggerName);
+            }
+        }
+
+        /// <summary>
+        /// Set robo to game mode or off game mode
+        /// </summary>
+        /// <param name="onOff"></param>
+        public void SetGameMode(bool onOff)
+        {
+            _roboAnimator.SetBool("Game", onOff);
+        }
+    }
+}
