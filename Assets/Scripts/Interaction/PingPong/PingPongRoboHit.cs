3e342fad108d04d613a61ad71472c49c7d096dfb
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-26 13:28:59 +0300

    Added roboforce

---
+++ Assets/Scripts/Interaction/PingPong/PingPongRoboHit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kekw.Interaction.PingPong
+{
+    class PingPongRoboHit:MonoBehaviour
+    {
+        protected readonly static float PINGPONG_BOT_FORCE = 2f;
+
+        Rigidbody _cache;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("PingBall"))
+            {
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * PINGPONG_BOT_FORCE, ForceMode.Impulse);
+            }
+        }
+    }
+}
