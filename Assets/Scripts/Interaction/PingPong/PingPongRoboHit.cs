aa11a9b38e43316c46540cf06953f6806693b54a
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-04-27 08:11:35 +0300

    Update PingPongRoboHit.cs

--- Assets/Scripts/Interaction/PingPong/PingPongRoboHit.cs
+++ Assets/Scripts/Interaction/PingPong/PingPongRoboHit.cs
@@ -10,12 +10,13 @@ namespace Kekw.Interaction.PingPong
         {
             if (collision.gameObject.CompareTag("PingBall"))
             { 
-                // Test stopping ball at robot and "hitting" ----->
+                // Stopping ball at robot ----->
                 Rigidbody ballBody = collision.gameObject.GetComponent<Rigidbody>();
                 ballBody.isKinematic = true;
                 ballBody.velocity = Vector3.zero;
                 ballBody.isKinematic = false;
-                // <----- Test stopping ball at robot and "hitting"
+                // <----- Stopping ball at robot
+                // "Hitting"
                 ballBody.AddForce(Vector3.forward * PINGPONG_BOT_FORCE, ForceMode.Impulse);
             }
         }
