895da622e26356286d7d5eda75983f3bd38cd9d8
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-05-03 12:04:22 +0300

    docs haptic update

--- Assets/Scripts/Interaction/Haptic/LeftHapticBroker.cs
+++ Assets/Scripts/Interaction/Haptic/LeftHapticBroker.cs
@@ -1,8 +1,12 @@
-
 namespace Kekw.Interaction
 {
     /// <summary>
-    /// Left hand controller haptic broker.
+    /// Left hand controller haptic broker.<br></br><br></br>
+    /// Can be found by type in scene.
+    /// <code>
+    ///  //Example usage how to trigger haptic.
+    ///  FindObjectOfType<LeftHapticBroker>().TriggerHapticFeedback(1f, 1f);
+    /// </code>
     /// </summary>
     public class LeftHapticBroker : AHapticBroker{} 
 }
