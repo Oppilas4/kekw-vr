efbf11213c8173aa8109dc1ebc74d03c0459ba85
monkeyman-69 <juho.lindemark@vuoksi.fi>
2023-05-04 13:09:07 +0300

    Added video data objects

---
+++ Assets/Scripts/Manager/VideoData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+namespace Kekw.Manager
+{
+    /// <summary>
+    /// Video data asset
+    /// </summary>
+    [CreateAssetMenu(fileName = "VideoData", menuName = "VideoData/videoContainer", order = 1)]
+    public class VideoData : ScriptableObject
+    {
+        /// <summary>
+        /// What video does this asset contain.
+        /// </summary>
+        public VideoClip _video;
+    }
+}
---
+++ Assets/Scripts/Manager/VideoManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+namespace Kekw.Manager
+{
+    /// <summary>
+    /// Set video clips before building build.
+    /// or playing game
+    /// </summary>
+    public class VideoManager : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("What video will be playing?")]
+        VideoData _videoData;
+
+        [SerializeField]
+        [Tooltip("Video player")]
+        VideoPlayer _videoPlayer;
+
+
+        private void Awake()
+        {
+            if(_videoData._video == null)
+            {
+                Debug.LogWarning(
+             
+                "Video is missing, correct this before building in build machine!\n" +
+                "Videos cannot be stored in version control. Import videos manually to project before build and set Video data container objects correctly in Assets/Video."
+                );
+                return;
+            }
+            _videoPlayer.clip = _videoData._video;
+        }
+    }
+}
