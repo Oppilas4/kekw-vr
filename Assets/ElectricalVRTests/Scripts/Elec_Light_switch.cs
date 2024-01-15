4a9e4aba5d663022fce775e7e9ec84d2b7c8c0ae
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-01-15 10:18:43 +0200

    LightSwitch Finished

--- Assets/ElectricalVRTests/Scripts/Elec_Light_switch.cs
+++ Assets/ElectricalVRTests/Scripts/Elec_Light_switch.cs
@@ -1,29 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Elec_Light_switch : MonoBehaviour
 {
     public GameObject on;
     public GameObject off;
-    public bool ison = false;
+    bool ison = false;
+    public UnityEvent WhatToWhenON,WhatToWhenOFF;
+    public AudioClip ClickSound;
+    AudioSource Audio;
     private void Start()
     {
-
+        Audio = GetComponent<AudioSource>();
     }
     public void OnTriggerEnter(Collider other)
     {
-            if (other.tag == "Player")
-            {                
+            if (other.tag == "LeftHand" || other.tag == "RightHand")
+            {
+                Audio.PlayOneShot(ClickSound);          
                 if (!ison)
                 {
+                    WhatToWhenON.Invoke();          
                     off.SetActive(false);
                     on.SetActive(true);
+                    ison = true;
                 }
                 else
                 {
+                    WhatToWhenOFF.Invoke();
                     off.SetActive(true);
                     on.SetActive(false);
+                    ison = false;
                 }
             }
     }
