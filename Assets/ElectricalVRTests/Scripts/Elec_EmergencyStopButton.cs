2f8b0b7e3f99137064be453f01feb93f238e3819
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-01-15 15:29:45 +0200

    some audio and memes

---
+++ Assets/ElectricalVRTests/Scripts/Elec_EmergencyStopButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Elec_EmergencyStopButton : MonoBehaviour
+{
+    public float threshold = 0.002f;
+    public Transform target;
+    public UnityEvent Reached;
+    private bool wasReached = false;
+    void FixedUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < threshold && !wasReached)
+        {
+            Reached.Invoke();
+            wasReached = true;
+        }
+        else if (distance >= threshold)
+        {
+            wasReached = false;
+        }
+    }
+}
