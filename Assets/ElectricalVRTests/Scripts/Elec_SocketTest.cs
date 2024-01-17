d837f4a215c40db25e1978074790719949992b8d
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-01-17 08:00:53 +0200

    I am in miseryy

---
+++ Assets/ElectricalVRTests/Scripts/Elec_SocketTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Elec_SocketTest : MonoBehaviour
+{
+    public XRSocketInteractor socketInteractor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
