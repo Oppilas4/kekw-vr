0998703d1b157e2e9db68852887cf589ed89c5c9
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-01-22 11:48:55 +0200

    Staplez

--- Assets/ElectricalVRTests/Scripts/Elec_StapleMakeStick.cs
+++ Assets/ElectricalVRTests/Scripts/Elec_StapleMakeStick.cs
@@ -2,8 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elec_StapleMakeStick : MonoBehaviour
+public class Elec_StapleMakeStick : MonoBehaviour, IVoltage
 {
+
+    public Elec_Voltage WireEndVolt;
+    public int startVoltage = 5;
+
+
+
+    private void Awake()
+    {
+        WireEndVolt = new Elec_Voltage(startVoltage);
+    }
+
+
+    public void Voltage_Receive(int newVoltage)
+    {
+        WireEndVolt.voltage = newVoltage;
+    }
+
+    public int Voltage_Send()
+    {
+        return WireEndVolt.voltage;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer != 13)
