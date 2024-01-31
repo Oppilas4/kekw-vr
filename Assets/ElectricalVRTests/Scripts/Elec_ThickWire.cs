68e21a04455a5b2160751cf17ff9548505602e7b
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-01-31 15:01:55 +0200

    stuff

---
+++ Assets/ElectricalVRTests/Scripts/Elec_ThickWire.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elec_ThickWire : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    private void Start()
+    {
+        lineRenderer = ;
+    }
+}
