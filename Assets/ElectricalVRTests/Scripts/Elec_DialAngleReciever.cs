01051f03b1ab8647227b66ee802a9f57ce0c234a
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-02-12 11:04:15 +0200

    yes

--- Assets/ElectricalVRTests/Scripts/Elec_DialAngleReciever.cs
+++ Assets/ElectricalVRTests/Scripts/Elec_DialAngleReciever.cs
@@ -6,11 +6,16 @@ using UnityEngine.Events;
 public class Elec_DialAngleReciever : MonoBehaviour, IElecDial
 {
     public UnityEvent OnReachedAngle;
+    public UnityEvent OnZeroDegree;
     public void DialChanged(float dialvalue)
     {
         if (dialvalue == 90)
         {
             OnReachedAngle.Invoke();
+        }
+        else if (dialvalue == 0)
+        {
+            OnZeroDegree.Invoke();
         }
     }
 }
