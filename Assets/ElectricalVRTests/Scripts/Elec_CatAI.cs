dc3f75cd14be7964b8cd89bf641e1999bcec98a6
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-02-14 09:27:37 +0200

    Rami Ridin Cat

--- Assets/ElectricalVRTests/Scripts/Elec_CatAI.cs
+++ Assets/ElectricalVRTests/Scripts/Elec_CatAI.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Elec_CatAI : MonoBehaviour
 {
     NavMeshAgent agent;
     public Transform Player;
-    public bool FelineIncstinctON;
     public Transform LaserPointerEnd;
     Animator animator;
     float Speed;
     public Transform Paws;
+    public GameObject RamiPos;
+
+    public Vector3 walkPoint;
+    public bool walkPointSet,FelineIncstinctON, RamiOn,RoutineGoing;
+    public float walkPointRange;
+    public LayerMask whatIsGround;
     private void Start()
     {
         animator = GetComponentInChildren<Animator>();
@@ -22,7 +28,11 @@ public class Elec_CatAI : MonoBehaviour
     {
         Speed = agent.velocity.magnitude;
         animator.SetFloat("Speed",Speed);
-        if (!FelineIncstinctON)
+        if (RamiOn)
+        {
+            Zoomies();
+        }
+        else if (!FelineIncstinctON)
         {
             agent.speed = 1.0f;
             agent.stoppingDistance = 2f;
@@ -39,5 +49,62 @@ public class Elec_CatAI : MonoBehaviour
                 animator.SetBool("CatchBool", true);
             }
         }       
+    }
+    private void Zoomies()
+    {
+        agent.speed = 3;
+        agent.stoppingDistance = 0;
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+
+        }
+           
+       
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        StartCoroutine(DontGetStuck());
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+    IEnumerator DontGetStuck()
+    {
+        if (!RoutineGoing)
+        {
+            RoutineGoing = true;
+            yield return new WaitForSeconds(5);
+            walkPointSet = false;
+            RoutineGoing = false;
+        }
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "ShredEye")
+        {
+            animator.SetTrigger("RamiOn");
+            other.GetComponent<XRBaseInteractable>().enabled = false;
+            other.gameObject.transform.parent = RamiPos.transform;
+            other.gameObject.transform.position = RamiPos.transform.position;
+            other.gameObject.transform.rotation = RamiPos.transform.rotation;
+            other.enabled = false;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.GetComponent<Animator>().SetTrigger("Ridin");           
+            RamiOn = true;
+        }
     }
 }
