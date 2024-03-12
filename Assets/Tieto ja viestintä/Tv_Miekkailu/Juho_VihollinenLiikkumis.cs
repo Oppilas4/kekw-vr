6445e393496874c8fed5864c951349a8c6246495
Santeri2 <santeri.suominen@vuoksi.fi>
2024-03-12 12:09:30 +0200

    asd

--- Assets/Tieto ja viestintä/Tv_Miekkailu/Juho_VihollinenLiikkumis.cs
+++ Assets/Tieto ja viestintä/Tv_Miekkailu/Juho_VihollinenLiikkumis.cs
@@ -2,25 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Juho_VihollinenLiikkumis : MonoBehaviour
 {
     public float speed;
     public Transform player;
     Juho_PelaajaHealth health;
+    public float range;
+    private Vector3 playerRange;
 
     private void Start()
     {
         player = GameObject.Find("XR Origin").GetComponent<Transform>();
         health = GameObject.Find("PlayerHealth").GetComponent<Juho_PelaajaHealth>();
+        playerRange = player.transform.position;
     }
 
     void Update()
     {
+
         Vector3 lookAt = player.position;
         lookAt.y = transform.position.y;
-        transform.LookAt(lookAt);
-        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= range)
+        {
+            transform.LookAt(lookAt);
+        }
+       
+        if (Vector3.Distance(transform.position, player.transform.position) > range)
+        {
+            transform.LookAt(lookAt);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
