c873067d834236ab828a3c85e4943676dbc47ad2
Ilona <venusflytrapd.d@gmail.com>
2024-03-12 10:45:15 +0200

    Audio Logic (for hair and garden)

---
+++ Assets/HS_HandSelectionManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HS_HandSelectionManager : MonoBehaviour
+{
+    [SerializeField] private XRRayInteractor _interactor;
+
+    private void Awake()
+    {
+        _interactor.selectEntered.AddListener(OnSelect);
+    }
+    public void OnSelect(SelectEnterEventArgs args)
+    {
+        Collider coll = args.interactableObject.colliders[0].GetComponent<Collider>();
+        if (coll.TryGetComponent<HS_IOnSelectAudioPlayer>(out HS_IOnSelectAudioPlayer component))
+        {
+        }
+
+    }
+
+}
