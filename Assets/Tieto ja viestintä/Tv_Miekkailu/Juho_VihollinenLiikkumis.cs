453afe907998a791954de9e6994b93a21d37dcb3
Santeri2 <santeri.suominen@vuoksi.fi>
2024-03-12 13:11:46 +0200

    123
    123

--- Assets/Tieto ja viestintä/Tv_Miekkailu/Juho_VihollinenLiikkumis.cs
+++ Assets/Tieto ja viestintä/Tv_Miekkailu/Juho_VihollinenLiikkumis.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
@@ -10,7 +11,15 @@ public class Juho_VihollinenLiikkumis : MonoBehaviour
     public Transform player;
     Juho_PelaajaHealth health;
     public float range;
+    public float detectionRange;
     private Vector3 playerRange;
+    public bool isRanged;
+
+
+
+    public GameObject enemyShootTransform;
+    public GameObject enemyBulletPrefab;
+    public float vitunforce;
 
     private void Start()
     {
@@ -22,28 +31,57 @@ public class Juho_VihollinenLiikkumis : MonoBehaviour
     void Update()
     {
 
-        Vector3 lookAt = player.position;
-        lookAt.y = transform.position.y;
-
-        if (Vector3.Distance(transform.position, player.transform.position) <= range)
+        if (Vector3.Distance(transform.position, player.transform.position) > detectionRange)
         {
-            transform.LookAt(lookAt);
-        }
-       
-        if (Vector3.Distance(transform.position, player.transform.position) > range)
-        {
-            transform.LookAt(lookAt);
-            transform.position += transform.forward * speed * Time.deltaTime;
+            EnnemyController();
         }
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             health.TakeDamage();
             Juho_VihollinenHeath healt = GetComponent<Juho_VihollinenHeath>();
             healt.TakeDamage(9999999);
         }
     }
+
+
+    void EnnemyController()
+    {
+
+        Vector3 lookAt = player.position;
+        lookAt.y = transform.position.y;
+
+        if (Vector3.Distance(transform.position, player.transform.position) > range)
+        {
+            transform.LookAt(lookAt);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= range)
+        {
+            if (isRanged)
+            {
+                StartCoroutine(attackDelay());
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    IEnumerator attackDelay()
+    {
+        GameObject projectileGO = (GameObject)Instantiate(enemyBulletPrefab, enemyShootTransform.transform.position,
+        enemyBulletPrefab.transform.rotation);
+        Rigidbody projectileRb = projectileGO.GetComponent<Rigidbody>();
+        projectileRb.AddForce(vitunforce * Vector3.forward, ForceMode.Impulse);
+        yield return new WaitForSeconds(2);
+    }
 }
+
+
+
