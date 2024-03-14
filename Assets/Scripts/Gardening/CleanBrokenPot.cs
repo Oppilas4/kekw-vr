f72dacd8476971fde72c1845ecd28a848c549510
Dmitrii <dmitrii.ivanov@vuoksi.fi>
2024-03-14 14:52:01 +0200

    Added cleaning after broken pot

--- Assets/Scripts/Gardening/CleanBrokenPot.cs
+++ Assets/Scripts/Gardening/CleanBrokenPot.cs
@@ -1,19 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gardening
 {
     public class CleanBrokenPot : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private List<GameObject> _potPieces = new List<GameObject>();
+        private float _delayBeforeCleaning = 1.5f;
+        private float _minTimeBeforeDeletion = .03f;
+        private float _maxTimeBeforeDeletion = .06f;
+
+        private void Start()
         {
-        
+
+            for (int i = 0; i < gameObject.transform.childCount; i++)
+            {
+                _potPieces.Add(gameObject.transform.GetChild(i).gameObject);
+            }
+            StartCoroutine(Clean());
         }
 
-        // Update is called once per frame
-        void Update()
+        private IEnumerator Clean()
         {
-        
+            yield return new WaitForSeconds(_delayBeforeCleaning);
+
+            int piecesCounter = 0;
+            while (piecesCounter < _potPieces.Count)
+            {
+                Destroy(_potPieces[piecesCounter]);
+                piecesCounter++;
+
+                yield return new WaitForSeconds(Random.Range(_minTimeBeforeDeletion, _maxTimeBeforeDeletion));
+            }
+
+            Destroy(gameObject);
         }
     }
 }
