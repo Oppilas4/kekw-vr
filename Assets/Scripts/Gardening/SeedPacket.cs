3bf1f1054143d8bc9edff5969807bb23abe2bf3c
Dmitrii <dmitrii.ivanov@vuoksi.fi>
2024-03-25 14:27:50 +0200

    Grabbing working

--- Assets/Scripts/Gardening/SeedPacket.cs
+++ Assets/Scripts/Gardening/SeedPacket.cs
@@ -14,6 +14,7 @@ namespace Gardening
             // Stops emission instead of turning off. This prevents delay on particle spawn
             // when re-enabling, as well as particles suddenly disappearing
             emission.enabled = CheckTilt();
+            Debug.Log(emission.enabled);
         }
 
         /// <summary>
@@ -24,7 +25,7 @@ namespace Gardening
             var angles = transform.rotation.eulerAngles;
             int dropAngle = 100;
             // Prevents the seed packet from acting weirdly with large rotation values
-            int modulo = 360 - dropAngle;
+            int modulo = 360 - dropAngle; 
             return Mathf.Abs(angles.x) % modulo >= dropAngle || Mathf.Abs(angles.z) % modulo >= dropAngle;
         }
     }
