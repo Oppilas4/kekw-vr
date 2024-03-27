442c1685dc0f37d3f29209bfcc162b6a956c5434
asorokin111 <129036204+asorokin111@users.noreply.github.com>
2024-03-27 12:27:14 +0200

    Automatic weather

--- Assets/Scripts/Gardening/WeatherManager.cs
+++ Assets/Scripts/Gardening/WeatherManager.cs
@@ -10,18 +10,38 @@ public class WeatherManager : MonoBehaviour
         Sunset,
         Rainy,
     }
+
+    [System.Serializable]
+    private struct WeatherData
+    {
+        public Material skybox;
+        public bool rainy;
+    }
+
+    // Workaround to show dictionary in inspector
     [System.Serializable]
     private struct AssociatedSkybox
     {
         public SkyboxWeather weather;
-        public Material material;
+        public WeatherData data;
     }
+
+    [Header("Skybox settings")]
     [SerializeField]
     private AssociatedSkybox[] _skyboxes;
-    private Dictionary<SkyboxWeather, Material> _skyboxMap = new();
+    private Dictionary<SkyboxWeather, WeatherData> _skyboxMap = new();
 
     [SerializeField]
     private SkyboxWeather _defaultSkybox;
+
+    [Header("Rain settings")]
+    [SerializeField]
+    private Transform _playerTransform;
+    [SerializeField]
+    private GameObject _rainPrefab;
+    [SerializeField]
+    private float _rainHeight;
+    private GameObject _rainInstance;
 
     private void Awake()
     {
@@ -33,17 +53,42 @@ public class WeatherManager : MonoBehaviour
         {
             instance = this;
         }
+        _playerTransform = GameObject.FindWithTag("Player").transform;
 
         foreach (var association in _skyboxes)
         {
-            _skyboxMap.Add(association.weather, association.material);
+            _skyboxMap.Add(association.weather, association.data);
         }
 
-        SetSkybox(_defaultSkybox);
+        SetWeather(_defaultSkybox);
     }
 
-    public void SetSkybox(SkyboxWeather weather)
+    public void SetWeather(SkyboxWeather weather)
     {
-        RenderSettings.skybox = _skyboxMap[weather];
+        RenderSettings.skybox = _skyboxMap[weather].skybox;
+        if (_skyboxMap[weather].rainy)
+        {
+            EnableRain();
+        }
+        else
+        {
+            DisableRain();
+        }
+    }
+
+    public void EnableRain()
+    {
+        if (_rainInstance == null)
+        {
+            _rainInstance = Instantiate(_rainPrefab, _playerTransform);
+            _rainInstance.transform.position += new Vector3(0, _rainHeight, 0);
+        }
+        _rainInstance.SetActive(true);
+    }
+
+    public void DisableRain()
+    {
+        if (_rainInstance == null) return;
+        _rainInstance.SetActive(false);
     }
 }
