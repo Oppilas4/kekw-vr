03a2f341f2a279431229e5b53796102540dd7304
asorokin111 <129036204+asorokin111@users.noreply.github.com>
2024-04-09 10:30:47 +0300

    Turned node count into a variable for readability and disabled alpha in blossom shader

--- Assets/Scripts/Gardening/IvyGenerator/Branch.cs
+++ Assets/Scripts/Gardening/IvyGenerator/Branch.cs
@@ -125,14 +125,15 @@ namespace Gardening
         private Mesh CreateMesh(List<IvyNode> nodes)
         {
             var sixMeshes = _meshFaces * 6;
+            int numberOfNodes = nodes.Count;
             Mesh branchMesh = new();
 
-            Vector3[] vertices = new Vector3[(nodes.Count) * _meshFaces * 4];
-            Vector3[] normals = new Vector3[nodes.Count * _meshFaces * 4];
-            Vector2[] uv = new Vector2[nodes.Count * _meshFaces * 4];
-            int[] triangles = new int[(nodes.Count - 1) * sixMeshes];
+            Vector3[] vertices = new Vector3[numberOfNodes * _meshFaces * 4];
+            Vector3[] normals = new Vector3[numberOfNodes * _meshFaces * 4];
+            Vector2[] uv = new Vector2[numberOfNodes * _meshFaces * 4];
+            int[] triangles = new int[(numberOfNodes - 1) * sixMeshes];
 
-            for (int index = 0; index < nodes.Count; index++)
+            for (int index = 0; index < numberOfNodes; index++)
             {
                 float vStep = 2f * Mathf.PI / _meshFaces;
 
@@ -171,11 +172,11 @@ namespace Gardening
                     var diff = pos - _branchNodes[index].GetPosition();
                     normals[index * _meshFaces + faceIndex] = diff / diff.magnitude;
 
-                    float uvID = Remap(index, 0, nodes.Count - 1, 0, 1);
+                    float uvID = Remap(index, 0, numberOfNodes - 1, 0, 1);
                     uv[index * _meshFaces + faceIndex] = new Vector2((float)faceIndex / _meshFaces, uvID);
                 }
 
-                if (index + 1 >= nodes.Count) continue;
+                if (index + 1 >= numberOfNodes) continue;
                 for (int faceIndex = 0; faceIndex < _meshFaces; faceIndex++)
                 {
                     var sixIndices = faceIndex * 6;
@@ -203,7 +204,6 @@ namespace Gardening
             Dictionary<int, Blossom> blossoms = new();
             for (int index = 0; index < nodes.Count; index++)
             {
-
                 var randomValue = Random.Range(0, 10);
 
                 if ((index <= 0 && !isFirst) || randomValue <= 2) continue;
