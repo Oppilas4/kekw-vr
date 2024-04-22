ca10ad6ae959580af1199c505c5a6b9a18f6cded
Official_Tuho <juhomarjakoski0@gmail.com>
2024-04-22 14:03:42 +0300

    Everything is so nice

--- Assets/Tieto ja viestintä/Tv_Koodi/The Scripts/TV_AnswerCube.cs
+++ Assets/Tieto ja viestintä/Tv_Koodi/The Scripts/TV_AnswerCube.cs
@@ -1,19 +1,13 @@
-using Oculus.Interaction;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class TV_AnswerCube : MonoBehaviour
 {
     public string koodiNimiKuutiolle;
-    public TMP_Text kuutionTeksti;
     public GameObject particleRefSystem;
     Vector3 resetTransform;
 
     public void Start()
     {
-        kuutionTeksti.text = koodiNimiKuutiolle;
         resetTransform = gameObject.transform.position;
     }
 
