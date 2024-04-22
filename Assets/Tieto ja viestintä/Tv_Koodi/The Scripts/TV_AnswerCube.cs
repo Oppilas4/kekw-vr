27db314580d0afcd640af32a42f07bbdd72b0e1d
Jami148 <133649186+Jami148@users.noreply.github.com>
2024-04-22 10:27:51 +0300

    mitävittua

--- Assets/Tieto ja viestintä/Tv_Koodi/The Scripts/TV_AnswerCube.cs
+++ Assets/Tieto ja viestintä/Tv_Koodi/The Scripts/TV_AnswerCube.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 public class TV_AnswerCube : MonoBehaviour
 {
     public string koodiNimiKuutiolle;
-   // public TMP_Text kuutionTeksti;
+    public TMP_Text kuutionTeksti;
     public GameObject particleRefSystem;
     Vector3 resetTransform;
 
     public void Start()
     {
+        kuutionTeksti.text = koodiNimiKuutiolle;
         resetTransform = gameObject.transform.position;
     }
 
