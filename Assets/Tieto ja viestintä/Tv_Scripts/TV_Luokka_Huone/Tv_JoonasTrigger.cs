9d89a0e3e4d4a729693efae147e63d88fd4dd724
Santeri2 <santeri.suominen@vuoksi.fi>
2024-05-03 10:26:54 +0300

    juu

--- Assets/Tieto ja viestintä/Tv_Scripts/TV_Luokka_Huone/Tv_JoonasTrigger.cs
+++ Assets/Tieto ja viestintä/Tv_Scripts/TV_Luokka_Huone/Tv_JoonasTrigger.cs
@@ -5,18 +5,15 @@ public class Tv_JoonasTrigger : MonoBehaviour
     public TV_CheckIfCompletedRise check;
     public TV_JoonasBehaviour joonas;
     bool hasTalked;
-    [SerializeField] AudioSource joonasAudio;
+    
 
-    private void Start()
-    {
-        joonasAudio.volume = .1f;
-    }
+   
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            joonasAudio.volume = 1f;
+            
 
             if (!hasTalked && check != null && check.hasTheNumber != 0)
             {
@@ -25,11 +22,5 @@ public class Tv_JoonasTrigger : MonoBehaviour
             }
         }
     }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            joonasAudio.volume = .1f;
-        }
-    }
+   
 }
