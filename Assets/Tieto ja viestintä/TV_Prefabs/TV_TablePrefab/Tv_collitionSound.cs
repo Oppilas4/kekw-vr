ed2e2042b4741f54db5dc2a17baa14ee34a64bdd
Official_Tuho <juhomarjakoski0@gmail.com>
2024-05-06 08:29:38 +0300

    What the fuck lol

--- Assets/Tieto ja viestintä/TV_Prefabs/TV_TablePrefab/Tv_collitionSound.cs
+++ Assets/Tieto ja viestintä/TV_Prefabs/TV_TablePrefab/Tv_collitionSound.cs
@@ -2,33 +2,27 @@ using UnityEngine;
 
 public class Tv_collitionSound : MonoBehaviour
 {
-    public AudioClip collisionSound; // Sound to play on collision
+    public AudioClip collisionSound;
     private AudioSource audioSource;
-
+    bool hasHitOnce = false;
     private void Start()
     {
-        // Get the AudioSource component attached to this GameObject
         audioSource = GetComponent<AudioSource>();
         if (audioSource == null)
         {
-            // If AudioSource is not attached, add it
             audioSource = gameObject.AddComponent<AudioSource>();
         }
 
-        // Assign the collision sound
         audioSource.clip = collisionSound;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Play the collision sound when collision occurs
-        audioSource.Play();
-    }
+        if (hasHitOnce)
+        {
+            audioSource.Play();
+        }
 
-    // If you're using triggers instead of collisions, use OnTriggerEnter
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     // Play the collision sound when trigger occurs
-    //     audioSource.Play();
-    // }
+        hasHitOnce = true;
+    }
 }
