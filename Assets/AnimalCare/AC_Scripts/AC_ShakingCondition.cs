3c09567131045a29584e432486be28df650ac3df
Oppilas4 <132988940+Oppilas4@users.noreply.github.com>
2025-03-05 13:03:17 +0200

    4

--- Assets/AnimalCare/AC_Scripts/AC_ShakingCondition.cs
+++ Assets/AnimalCare/AC_Scripts/AC_ShakingCondition.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class AC_ShakingCondition : MonoBehaviour
 {
     public float forceSpeed = 2f;
+
+    public Animator dogAnimator;             // Reference to the dog's Animator
+
+    public string shakeAnimationTrigger = "Shake"; // The trigger to start the shake animation
+
+    public void Start()
+    {
+        if (dogAnimator == null)
+        {
+            dogAnimator = GetComponent<Animator>(); // Assign the Animator if not set in the inspector
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
+        
         Rigidbody StuffRigidbody = collision.gameObject.GetComponent<Rigidbody>();
         StuffRigidbody.AddForce(transform.right * forceSpeed, ForceMode.Impulse);
+        dogAnimator.SetTrigger(shakeAnimationTrigger);
     }
 }
